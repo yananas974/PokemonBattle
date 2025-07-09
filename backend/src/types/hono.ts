7919afb1e36ba87e815ce82450d60526e5f4cb34
[Yann Baraguay] <[yann.baraguay@gmail.com]>
2025-07-09 15:23:03 +0200

    refactor: delete dossier inutiliser

--- backend/src/types/hono.ts
+++ backend/src/types/hono.ts
@@ -1,5 +1,5 @@
 import type { Context } from 'hono';
-import type { User } from '../models/interfaces/user.interface.js';
+import type { User } from '@pokemon-battle/shared';
 
 export interface AuthContext extends Context {
   get(key: 'user'): User;
